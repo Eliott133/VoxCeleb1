7b4d2200a8bba8afd20adeb3d0d255d8c335b77c
Eliott <mauberteliott@gmail.com>
2024-12-05 15:21:46 +0100

    deleting useless comments

--- app/services/filterJoin.py
+++ app/services/filterJoin.py
@@ -2,14 +2,12 @@ from app.services.filterGender import encode_genre
 from app.services.filterLength import encode_length
 import numpy as np
 import pandas as pd
-#pandas version '2.2.3' 
 
 #Permet de faire un trie avec plusieurs elements 
 def filterJoin(meta,data,file,genre=None,seuil=None,path_audio=""):
     modif=False
     if genre is not None :
         modif=True
-        ##encode_genre(meta,data,genre,file=file)
         data = encode_genre(meta,data,genre,file=file)
     if seuil is not None :
         modif=True
@@ -18,6 +16,3 @@ def filterJoin(meta,data,file,genre=None,seuil=None,path_audio=""):
         data.to_pickle(file)
     data.to_pickle(file)
     return data
-
-
-#filterJoin(meta,data,"../data/data/voxceleb1/trainTcM.pkl",genre='M',seuil='tl',path_audio='../data/data/voxceleb1/')
