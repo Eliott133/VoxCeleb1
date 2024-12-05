a897a019d74c1e0630de80ab58218d0885696b04
Eliott <mauberteliott@gmail.com>
2024-12-05 15:07:38 +0100

    added function to format a date in file

--- app/static/js/utils/times.js
+++ app/static/js/utils/times.js
@@ -3,3 +3,8 @@ function formatTime(seconds) {
     const secs = Math.floor(seconds % 60);
     return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
 }
+
+function formatDateTime(date){
+    return (date.getDate()+"_"+date.getMonth()+"_"+date.getFullYear()+"_"+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());
+}
+
