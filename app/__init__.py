e959055952627fe5e9414e7b509a2680539751a5
Eliott <mauberteliott@gmail.com>
2024-12-16 14:52:56 +0100

    (refactor)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from app.routes.filters import filters_bp
 from app.routes.audio import audio_bp
+from app.extensions import socketio
 
 def create_app():
     app = Flask(__name__)
@@ -9,4 +10,5 @@ def create_app():
     app.register_blueprint(filters_bp)
     app.register_blueprint(audio_bp)
 
+    socketio.init_app(app)  # Initialisation de SocketIO avec Flask
     return app
